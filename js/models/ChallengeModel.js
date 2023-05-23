672bebbfb2e16c9d54450315a165812f9f7cc9da
Francisco Gameiro <gameirofrancisco6@gmail.com>
2023-05-23 22:38:04 +0100

    Aprimoramento do modelo Challenge

--- js/models/ChallengeModel.js
+++ js/models/ChallengeModel.js
@@ -2,14 +2,36 @@ class Challenge{
     title = ''
     type = ''
 
-    constructor(title, type){
+    //NOTA: Fill-in-blanks
+    fibText = ['']
+    fibAnswers = [[]]
+
+    //NOTA: Quiz
+    quizText = ['']
+    quizAnswers = [[[],0]] //*1
+
+    //NOTA: Resposta símples
+    simText = ['']
+    simAnswer = ['']
+
+    constructor(title, type, fibText = [''], fibAnswers = [[]], quizText = [''], quizAnswers = [[[],0]], simText = [''], simAnswer = ['']){ //*1
         this.title = title
         this.type = type
 
         switch (type){
-            case ('fill-in-blanks'):    //TODO: Acrescentar condições em cada tipo de atividade.
+            //TODO: Acrescentar condições em cada tipo de atividade.
+            case ('fill-in-blanks'):    this.fibText = fibText
+                                        this.fibAnswers = fibAnswers
                                         break;
-            default: console.log(`"${type}" is not a valid activity type.`)
+
+            case ('quiz'):  this.quizText = quizText
+                            this.quizAnswers = quizAnswers //TODO: Ponderar como fazer a parte do quiz [*1]
+                            break;
+            
+            case ('simple'):    this.simText = simText
+                                this.simAnswer = simAnswer
+
+            default:    console.log(`"${type}" is not a valid activity type.`)
         }
     }
 }
