6192c767088daf00d4c06a9e239e9642b54428f1
BensonTerra <benson.terra@hotmail.com>
2023-05-23 15:40:22 +0100

    teste

--- js/init.js
+++ js/init.js
@@ -1,5 +1,7 @@
 import { initDataUsers } from "./models/UsersModel.js"
 console.log("init.js")
+
 console.log("Available Height: " + window.screen.availHeight);
 console.log("Available Width: " + window.screen.availWidth);
+
 initDataUsers()
