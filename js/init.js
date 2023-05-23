ac2d2e246abb3b675c5f1288b22a602874234d9e
BensonTerra <benson.terra@hotmail.com>
2023-05-23 15:34:35 +0100

    sqwde

--- js/init.js
+++ js/init.js
@@ -1,3 +1,5 @@
 import { initDataUsers } from "./models/UsersModel.js"
 console.log("init.js")
+console.log("Available Height: " + window.screen.availHeight);
+console.log("Available Width: " + window.screen.availWidth);
 initDataUsers()
