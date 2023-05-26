ed89136470405b16a87810bde2c0e499c211ba30
BensonTerra <benson.terra@hotmail.com>
2023-05-26 20:33:58 +0100

    avalaia francisco

--- js/views/contentFotterView.js
+++ js/views/contentFotterView.js
@@ -6,18 +6,32 @@ function contentBuild()
 
     result += 
     `
-    <div class="container-fluid p-5">
-        <div class="row">
-            <div class="align-items-center">
-                <img src="/img/Logo_Principal_Mono.png" class="img-fluid">
-            </div>
-            <div class="align-items-center">
-                <h1>Welcome</h1>
+    <div class="container">
+        <div class="container">
+            <div class="row align-items-center">
+                <div class="col-lg-6 col-md-6 mb-4 mb-md-0">
+                    <img src="/img/Logo_Principal_mono.png" id="imgCustom"/>
+                </div>
+                <div class="col-lg-6">
+                    <p class="text-lg-start">
+                        “Insert Coin” é um website feito no âmbito do projeto interdisciplinar do 1º ano da Licenciatura em Tecnologias e Sistemas de Informação para a Web da Escola Superior de Media Artes e Design da Politécnico do Porto.
+                        Consiste na criação de uma Escape-Room educacional
+                        Este projeto é feito por:
+                    </p>
+                    <ul class="list-styled text-lg-start">
+                        <li class="mb-2">
+                            Benson Terra
+                        </li>
+                        <li class="mb-2">
+                            Francisco Gameiro
+                        </li>
+                        <li class="mb-2">
+                            Joana Bastos
+                        </li>
+                    </ul>
+                </div>
             </div>
         </div>
-
-        
-
     </div>
     `
 
