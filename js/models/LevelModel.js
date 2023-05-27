e03ace14376dad502552a58e23d063e24277a52d
FranciscoGameiro2004 <40220426@esmad.ipp.pt>
2023-05-27 19:52:14 +0100

    Alterar LevelModel

--- js/models/LevelModel.js
+++ js/models/LevelModel.js
@@ -12,16 +12,28 @@ class Level{
     defaultMaps = ['']
     alternateMaps = [['']]
 
-    constructor(title, thumbnail, thumbnailLocked = '', timeInSeconds, challenges, link = '', defaultViews=[''], alternateViews=[['']], defaultMaps=[''], alternateMaps=['']){
+    defaultPreRequisite = ['']
+    alternatePreRequisite = [['']]
+
+    constructor(title, thumbnail, thumbnailLocked = '', timeInSeconds, challenges, link = '', defaultViews=[''], alternateViews=[['']], defaultMaps=[''], alternateMaps=[['']], defaultPreRequisite=[''], alternatePreRequisite=[['']]){
         this.title = title
         this.thumbnail = thumbnail
         this.thumbnailLocked = thumbnailLocked
         this.timeInSeconds = timeInSeconds
         this.challenges = challenges
         this.link = link
+
+        this.defaultViews = defaultViews
+        this.alternateViews = alternateViews
+
+        this.defaultMaps = defaultMaps
+        this.alternateMaps = alternateMaps
+
+        this.defaultPreRequisite = defaultPreRequisite
+        this.alternatePreRequisite = alternatePreRequisite
     }
 }
 
-export function createLevel(title='', thumbnail='', thumbnailLocked = '', timeInSeconds = 0, challenges = [], link = '#', defaultViews=[''], alternateViews=[['']], defaultMaps=[''], alternateMaps=['']){
-    return new Level(title, thumbnail, thumbnailLocked, timeInSeconds, challenges, link, defaultViews, alternateViews, defaultMaps, alternateMaps)
+export function createLevel(title='', thumbnail='', thumbnailLocked = '', timeInSeconds = 0, challenges = [], link = '#', defaultViews=[''], alternateViews=[['']], defaultMaps=[''], alternateMaps=[['']], defaultPreRequisite=[''], alternatePreRequisite=[['']]){
+    return new Level(title, thumbnail, thumbnailLocked, timeInSeconds, challenges, link, defaultViews, alternateViews, defaultMaps, alternateMaps, defaultPreRequisite, alternatePreRequisite)
 }
