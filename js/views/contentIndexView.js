6c20388e5b02ed62337a757908bfc680940ca393
BensonTerra <benson.terra@hotmail.com>
2023-05-28 17:00:38 +0100

    sinopser

--- js/views/contentIndexView.js
+++ js/views/contentIndexView.js
@@ -19,12 +19,42 @@ function contentBuild()
     </div>
     `
 
-    document.querySelector("#content").innerHTML = result
+    document.querySelector("#contentPrincipal").innerHTML = result
 
     if(sessionStorage.getItem('userLogged')){
         const btn = document.querySelector('#btnIndex')
         btn.innerHTML = 'Jogar'
         btn.setAttribute('href', './html/levelSelection.html')
     }
+
+    result = ""
+    result += 
+    `
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12 mt-3">
+                <div class="card m-10  border-white">
+                    <div class="card-horizontal">
+
+                        <div class="card-body">
+                            <h4 class="card-title text-start">Sinopse</h4>
+                            <p class="card-text text-start">
+                                A aventura começa quando vais para um museu de história dos videojogos. 
+                                De repente, tu ficas trancado em um conjunto de salas cheias de relíquias da história dos arcades. 
+                                Para sair do museu, terás de resolver diversos desafios em salas que remetem tópicos desde o surgimento dos arcades, à sua era dourada até á sua decadência. 
+                            </p>
+                        </div>
+
+                        <div class="img-square-wrapper p-5">
+                            <img class="" src="http://via.placeholder.com/300x300" alt="Card image cap">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    `
+
+    document.querySelector("#contentSinopse").innerHTML = result
 }
 contentBuild()
