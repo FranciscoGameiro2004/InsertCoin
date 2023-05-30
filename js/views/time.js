10214342ed54e524102b4265721e66c0b40eb11a
BensonTerra <benson.terra@hotmail.com>
2023-05-30 17:34:15 +0100

    save

--- js/views/time.js
+++ js/views/time.js
@@ -1,9 +1,32 @@
 const button = document.querySelector("#restart-button");
-const bars = document.querySelectorAll(".round-time-bar");
-button.addEventListener("click", () => {
-  bars.forEach((bar) => {
-    bar.classList.remove("round-time-bar");
-    bar.offsetWidth;
-    bar.classList.add("round-time-bar");
-  });
+const bar = document.querySelector(".round-time-bar");
+const salaTime = JSON.parse(localStorage.getItem("levels"))[0].timeInSeconds
+console.log(salaTime);
+
+let time = null
+let checkStart = false
+
+button.addEventListener("click", () => 
+{
+  if(checkStart == false)
+  {
+    startTimer()
+    checkStart = true;
+  }
 });
+
+function startTimer()
+{
+  bar.style.setProperty('--duration', salaTime)
+  bar.classList.remove("round-time-bar");
+  bar.offsetWidth;
+  bar.classList.add("round-time-bar");
+
+
+  time = setTimeout(() =>
+  {
+    checkStart = false
+    console.log("end timer");
+  },salaTime*1000)
+}
+
