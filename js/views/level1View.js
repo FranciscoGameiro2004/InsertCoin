ec6d1729f8fb73524d40ac46e8a3f29d11c46f4e
BensonTerra <benson.terra@hotmail.com>
2023-06-01 15:59:32 +0100

    save

--- js/views/level1View.js
+++ js/views/level1View.js
@@ -1,15 +1,36 @@
 const leftArrow = document.getElementById('leftArrow');
-console.log(leftArrow);
+//console.log(leftArrow);
 const rightArrow = document.getElementById('rightArrow');
-console.log(rightArrow);
+//console.log(rightArrow);
+const btn1 = document.getElementById('btn1');
+//console.log(btn1);
+const gameScreen = document.getElementById('gameScreen');
+console.log(gameScreen);
 
-function teste()
+let salaViewsDefault = JSON.parse(localStorage.getItem("levels"))[0].defaultViews
+console.table(salaViewsDefault)
+
+let indexView = 0
+gameScreen.style.backgroundImage = `url(${salaViewsDefault[indexView]})`
+
+leftArrow.addEventListener('click', () => 
 {
-    console.log('teste');
-}
+    indexView -= 1
+    console.log("leftArrow")
+    gameScreen.style.backgroundImage = `url(${salaViewsDefault[indexView]})`
+});
+rightArrow.addEventListener('click',() => 
+{
+    indexView += 1
+    console.log("rightArrow")
+    gameScreen.style.backgroundImage = `url(${salaViewsDefault[indexView]})`
+});
+/*
+btn1.addEventListener('click',() => 
+{
+    gameScreen.style.backgroundImage = "url(" + salaViewsDefault[]
+})
+*/
 
 
-
-leftArrow.addEventListener('click', () => {console.log("leftArrow")});
-rightArrow.addEventListener('click',() => {console.log("rightArrow")});
 
--- js/views/time.js
+++ js/views/time.js
@@ -1,15 +1,20 @@
-const button = document.querySelector("#restart-button");
+const startBtn = document.querySelector("#start-button");
+const stopBtn = document.querySelector("#stop-button");
 const bar = document.querySelector("#timeBar");
 const salaTime = JSON.parse(localStorage.getItem("levels"))[0].timeInSeconds
 console.log(salaTime);
 
 let time = null
+let checkTime = null
 let checkStart = false
+let checkEnd = false
+let check = 0
 
-button.addEventListener("click", () => 
+startBtn.addEventListener("click", () => 
 {
   if(checkStart == false)
   {
+    check = 0
     startTimer()
     checkStart = true;
   }
@@ -17,29 +22,41 @@ button.addEventListener("click", () =>
 
 function startTimer()
 {
+    document.querySelector("#timeBar2").style.animationPlayState = 'running'
     bar.style.setProperty('--duration', salaTime)
     bar.classList.remove("round-time-bar");
     bar.offsetWidth;
     bar.classList.add("round-time-bar");
-    teste(salaTime)
+    controlTimer(salaTime)
 
     time = setTimeout(() =>
     {
         checkStart = false
-        console.log("end timer");
     },salaTime*1000)
 }
 
-function teste(time)
+stopBtn.addEventListener("click", () => 
+{
+    checkEnd = true
+    checkStart = false
+    document.querySelector("#timeBar2").style.animationPlayState = 'paused'
+    clearInterval(checkTime);
+    console.log(check)
+})
+
+function controlTimer(time)
 {
     console.log(time)
-    let checkTime = setInterval(function () 
+    checkTime = setInterval(function () 
     {
-        time -= 1
-        console.log(time)
-        if (time == 0)
+        check += 1
+        console.log(check)
+        if (check == time)
         {
             clearInterval(checkTime);
+            console.log("end timer");
+            console.log(check)
+            checkEnd = false
         }
     }, 1000);
 }
