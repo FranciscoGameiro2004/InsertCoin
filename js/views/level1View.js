0d320538f42ac6012b54543ad1fc9ba641628764
BensonTerra <benson.terra@hotmail.com>
2023-06-02 11:57:41 +0100

    exemplo de resize

--- js/views/level1View.js
+++ js/views/level1View.js
@@ -4,11 +4,15 @@ const rightArrow = document.getElementById('rightArrow');
 //console.log(rightArrow);
 const btn1 = document.getElementById('btn1');
 //console.log(btn1);
+const btn2 = document.getElementById('btn2');
+//console.log(btn2);
+const btnReset = document.getElementById('reset');
+//console.log(btnReset);
 const gameScreen = document.getElementById('gameScreen');
 console.log(gameScreen);
 
 let salaViewsDefault = JSON.parse(localStorage.getItem("levels"))[0].defaultViews
-console.table(salaViewsDefault)
+console.log(salaViewsDefault)
 
 let indexView = 0
 gameScreen.setAttribute('src', `${salaViewsDefault[indexView]}`) 
@@ -40,9 +44,35 @@ function checkIndex()
     }
 }
 
-console.log("Available Width: " + window.screen.availWidth);
-console.log("Available Height: " + window.screen.availHeight);
+btn1.addEventListener('click',() =>
+{
+    salaViewsDefault[1] = "../img/DefaultRoomAssets/1/AlternativeViews/View2/View2_1.jpg"
+    render()
+})
+btn2.addEventListener('click',() =>
+{
+    salaViewsDefault[1] = "../img/DefaultRoomAssets/1/AlternativeViews/View2/View2_2.jpg"
+    render()
+})
+btnReset.addEventListener('click',() =>
+{
+    const defaultViews =
+    [
+        "../img/DefaultRoomAssets/1/DefaultViews/View1.jpg",
+        "../img/DefaultRoomAssets/1/DefaultViews/View2.jpg",
+        "../img/DefaultRoomAssets/1/DefaultViews/View3.jpg",
+        "../img/DefaultRoomAssets/1/DefaultViews/View4.jpg"
+    ]
+    salaViewsDefault = defaultViews
+    //console.log(salaViewsDefault)
+    render()
+})
 
-
-
-
+function render()
+{
+    gameScreen.setAttribute('src', `${salaViewsDefault[indexView]}`) 
+}
+/*
+console.log("gameScreen width: " + gameScreen.offsetWidth);
+console.log("gameScreen height: " + gameScreen.offsetHeight);
+*/
