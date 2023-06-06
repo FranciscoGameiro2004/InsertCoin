a685e87ea9cb34d3d7979dd662f620a3aa40c4c5
BensonTerra <benson.terra@hotmail.com>
2023-06-06 16:18:37 +0100

    save

--- js/views/level1View.js
+++ js/views/level1View.js
@@ -10,6 +10,8 @@ const btnReset = document.getElementById('reset');
 //console.log(btnReset);
 const gameScreen = document.getElementById('gameScreen');
 console.log(gameScreen);
+console.log(gameScreen.offsetWidth)
+console.log(gameScreen.offsetHeight)
 
 let salaViewsDefault = JSON.parse(localStorage.getItem("levels"))[0].defaultViews
 console.log(salaViewsDefault)
@@ -76,3 +78,109 @@ function render()
 console.log("gameScreen width: " + gameScreen.offsetWidth);
 console.log("gameScreen height: " + gameScreen.offsetHeight);
 */
+resizeMap();
+function resizeMap() 
+{
+    // Obtém a imagem e o mapa
+    var imagem = document.getElementById('gameScreen');
+    var mapa = document.querySelector('map');
+
+    // Função para redimensionar o mapa
+    function resizeMap() 
+    {
+      console.log(imagem.naturalWidth)
+      console.log(imagem.naturalHeight)
+
+      var widthRatio = imagem.clientWidth / imagem.naturalWidth;
+      var heightRatio = imagem.clientHeight / imagem.naturalHeight;
+
+      // Redimensiona as coordenadas de cada área do mapa
+      var areas = mapa.getElementsByTagName('area');
+      for (var i = 0; i < areas.length; i++) 
+      {
+        var coords = areas[i].getAttribute('coords').split(',');
+        for (var j = 0; j < coords.length; j++) 
+        {
+            console.log(coords)
+            coords[j] = Math.round(parseInt(coords[j]) * widthRatio);
+        }
+        areas[i].setAttribute('coords', coords.join(','));
+      }
+      //console.log(`widthRatio:${widthRatio}||heightRatio:${heightRatio}`)
+      //console.log(coords)
+    }
+    // Redimensiona o mapa quando a imagem for carregada e quando a janela for redimensionada
+    imagem.addEventListener('load', resizeMap);
+
+    // Redimensiona o mapa pela primeira vez
+    window.onresize = resizeMap;
+}
+
+
+
+// Function to create points on the game screen image
+function createPoints() 
+{
+    // Get the image map element
+    const view1 = document.getElementById("view1");
+
+    // Get the areas (image maps) within the image map
+    const areas = view1.getElementsByTagName("area");
+
+    // Get the point container element
+    const pointContainer = document.getElementById("pointContainer");
+
+    const timeBar = 25
+
+    // Loop through the areas and create points for each coordinate
+    for (let i = 0; i < areas.length; i++) 
+    {
+        //console.log("ciclo " + i)
+        const area = areas[i];
+        let coords = area.getAttribute("coords").split(",");
+        //console.log(coords.length)
+        let cordsLength = coords.length/2
+
+        for (let j = 0; j < cordsLength; j++)
+        {
+            /*
+            console.log("-----------------------------------------")
+            console.log("-------------cilco:" + j + "-------------")
+            console.log("-----------------------------------------")
+            console.log(coords)
+            */
+            let newCoordsPoints = coords.splice(0,2)
+            //console.log(coords)
+            //console.log(newCoordsPoints)
+            const point = document.createElement("div");
+
+            // Set the position and style of the point
+            point.className = "point";
+            point.style.left = newCoordsPoints[0] + "px";
+            let newCoord = stringToInt(newCoordsPoints[1]) + timeBar;
+            //console.log(newCoord)
+            newCoordsPoints[1] = intToString(newCoord);
+            point.style.top = newCoordsPoints[1] + "px";
+
+            // Add the point to the point container
+            pointContainer.appendChild(point);
+        }
+
+    
+    }
+}
+
+// Call the createPoints function when the page has finished loading
+window.addEventListener("load", createPoints);
+
+function stringToInt(str) 
+{
+return parseInt(str, 10);
+}
+
+function intToString(num) 
+{
+return num.toString();
+}
+
+
