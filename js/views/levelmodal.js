5259cfb14719e5116a5bedf0d93576d6875d7848
FranciscoGameiro2004 <40220426@esmad.ipp.pt>
2023-06-19 23:55:51 +0100

    Correções respostas símples

--- js/views/levelmodal.js
+++ js/views/levelmodal.js
@@ -105,8 +105,8 @@ function checkRes()
 {
     if(typeModal == "simple")
     {
-        resUser = resQuestion
-       //resUser = document.getElementById("simAnswer").value
+        //resUser = resQuestion
+       resUser = document.getElementById("simAnswer").value
     }
     //console.log(`${resQuestion} || ${resUser} `)
     if(resQuestion.toLowerCase() == resUser.toLowerCase())
